c9834d8e6bcd7a8b888110ec28a0a508953d85de
Bhumesh_Kewat <96462127+Bhumesh2001@users.noreply.github.com>
2024-02-10 23:47:53 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,11 +1,44 @@
-const apiKey = '';
-const searchToNews = 'football';
-const url = `https://newsapi.org/v2/everything?q=${searchToNews}&apiKey=${apiKey}`;
-fetch(url)
-    .then(response => response.json())
-    .then(data => {
-        console.log(data.articles);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
+document.addEventListener('DOMContentLoaded', () => {
+    let searchToNews = 'Latest News';
+    const fetchAndDisplayNews = () => {
+        const apiKey = '';
+        const flexContainer = document.querySelector('.flex-container');
+        const heading = document.querySelector('.heading');
+        const url = `https://newsapi.org/v2/everything?q=${searchToNews}&apiKey=${apiKey}`;
+        flexContainer.innerHTML = '';
+        heading.textContent = searchToNews;
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                data.articles.forEach(article => {
+                    if (article.title && article.description && article.urlToImage) {
+                        const flex_box_div = document.createElement('div');
+                        const imgTag = document.createElement('img');
+                        const h3Tag = document.createElement('h3');
+                        const ptag = document.createElement('p');
+
+                        flex_box_div.className = 'flex-box';
+                        imgTag.src = article.urlToImage;
+                        h3Tag.textContent = article.title;
+                        ptag.textContent = article.description;
+
+                        flex_box_div.appendChild(imgTag);
+                        flex_box_div.appendChild(h3Tag);
+                        flex_box_div.appendChild(ptag);
+                        flexContainer.appendChild(flex_box_div);
+                    };
+                });
+            })
+            .catch(error => {
+                console.error('Error fetching data:', error);
+            });
+    };
+    fetchAndDisplayNews();
+    const h4Elements = document.querySelectorAll('h4');
+    h4Elements.forEach(h4 => {
+        h4.addEventListener('click', () => {
+            searchToNews = h4.textContent.toString();
+            fetchAndDisplayNews();
+        });
     });
+});
